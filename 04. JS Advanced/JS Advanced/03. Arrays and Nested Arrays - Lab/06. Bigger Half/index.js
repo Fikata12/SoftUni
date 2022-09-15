a1289e3ae3426e4cf8109f0641f3c37606440a77
Fikata12 <filipkarabashev@gmail.com>
2022-09-16 02:10:02 +0300

    Add exercises from 03. Arrays and Nested Arrays - Lab

--- 04. JS Advanced/JS Advanced/03. Arrays and Nested Arrays - Lab/06. Bigger Half/index.js
+++ 04. JS Advanced/JS Advanced/03. Arrays and Nested Arrays - Lab/06. Bigger Half/index.js
@@ -1,5 +1,5 @@
 function sort(input) {
     let array = [...input];
     array.sort((a, b) => a - b);
-    return array.splice(Math.floor(array.length / 2), array.length - 1);
+    return array.slice(Math.floor(array.length / 2));
 }
