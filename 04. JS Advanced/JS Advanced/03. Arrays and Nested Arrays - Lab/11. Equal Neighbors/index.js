518c6641fe1e6d139bb0d477706b3870b67ceafd
Fikata12 <filipkarabashev@gmail.com>
2022-09-16 03:49:16 +0300

    Add exercises from 03. Arrays and Nested Arrays - Lab

---
+++ 04. JS Advanced/JS Advanced/03. Arrays and Nested Arrays - Lab/11. Equal Neighbors/index.js
@@ -0,0 +1,19 @@
+function solve(input) {
+    let matrix = [...input];
+    let counter = 0;
+    for (let i = 0; i < matrix.length - 1; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == matrix[i + 1][j]) {
+                counter++;
+            }
+        }
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length - 1; j++) {
+            if (matrix[i][j] == matrix[i][j + 1]) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
