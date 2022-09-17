eddfcafeaa9ef76b1ab166e4a06d7925dd2de084
Fikata12 <filipkarabashev@gmail.com>
2022-09-17 03:09:24 +0300

    Add exercises from 04. Arrays and Nested Arrays - Exercise

---
+++ 04. JS Advanced/JS Advanced/04. Arrays and Nested Arrays - Exercise/09. Magic Matrices/index.js
@@ -0,0 +1,27 @@
+function func(inputArray) {
+    let array = [...inputArray];
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (i == 0) {
+            sum = Sum(array[i]);
+        }
+        else if (sum != Sum(array[i])) {
+            console.log('false');
+            return;
+        }
+    }
+    for (let i = 0; i < array.length; i++) {
+        let currentSum = 0;
+        for (let j = 0; j < array.length; j++) {
+            currentSum += array[j][i];
+        }
+        if (sum != currentSum) {
+            console.log('false');
+            return;
+        }
+    }
+    console.log('true');
+    function Sum(array) {
+        return array.reduce((total, current) => { return total += current });
+    }
+}   
