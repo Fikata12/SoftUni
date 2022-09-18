4710cb4bb255459ac5058dd971cfe1fb1b2e073d
Fikata12 <filipkarabashev@gmail.com>
2022-09-18 23:56:31 +0300

    Add exercises from 04. Arrays and Nested Arrays - Exercise

---
+++ 04. JS Advanced/JS Advanced/04. Arrays and Nested Arrays - Exercise/10. Tic-Tac-Toe/index.js
@@ -0,0 +1,11 @@
+function func(inputArray) {
+    let array = [...inputArray];
+    let dashboard = [[], [], []];
+    dashboard.forEach(element => {
+        element.fill(false, 0, 2);
+    });
+    for (const coordinates of array) {
+        
+    }
+}
+func()
