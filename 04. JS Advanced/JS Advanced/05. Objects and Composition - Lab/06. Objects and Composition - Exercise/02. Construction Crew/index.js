f83a0a09751b0db0bc9c995672e50272ae50c48f
Fikata12 <filipkarabashev@gmail.com>
2022-09-22 01:17:48 +0300

    Add exercises from 06. Objects and Composition - Exercise

---
+++ 04. JS Advanced/JS Advanced/05. Objects and Composition - Lab/06. Objects and Composition - Exercise/02. Construction Crew/index.js
@@ -0,0 +1,7 @@
+function func(worker) {
+    if (worker.dizziness == true) {
+        worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+        worker.dizziness = false;
+    }
+    return worker;
+}
