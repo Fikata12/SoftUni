b2f412e6a94a68cb124044a1fb4d9032dfe1007a
Fikata12 <filipkarabashev@gmail.com>
2022-10-15 02:31:25 +0300

    Add exercises from 13. Unit Testing and Error Handling - Lab

--- 04. JS Advanced/JS Advanced/13. Unit Testing and Error Handling - Lab/04. Sum of Numbers/test/sum.test.js
+++ 04. JS Advanced/JS Advanced/13. Unit Testing and Error Handling - Lab/04. Sum of Numbers/test/sum.test.js
@@ -2,9 +2,23 @@ const { expect } = require("chai");
 const { sum } = require("..");
 describe('Tests', () => {
     it('should return sum if input is valid', () => {
-        expect(sum([1, 2, 3])).to.be.equal(6);
+        // Arrange
+        let input = [1, 2, 3];
+
+        // Act
+        let result = sum(input);
+
+        // Assert
+        expect(result).to.be.equal(6);
     });
     it('should return NaN if input is invalid', () => {
-        expect(sum([1, 2, 'a'])).to.be.NaN;
+        // Arrange
+        let input = [1, 2, 'a'];
+
+        // Act
+        let result = sum(input);
+
+        // Assert
+        expect(result).to.be.NaN;
     });
 });
