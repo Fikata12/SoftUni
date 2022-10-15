f7b68236b8ab4158d1447e7f062f8041a5d9306d
Fikata12 <filipkarabashev@gmail.com>
2022-10-15 04:27:34 +0300

    Add exercises from 14. Unit Testing and Error Handling - Exercise

---
+++ 04. JS Advanced/JS Advanced/14. Unit Testing and Error Handling - Exercise/01. Request Validator/index.js
@@ -0,0 +1,22 @@
+function solve(obj) {
+    let validMethods = ['GET', 'POST', 'DELETE', 'CONNECT'];
+    let validUri = /^[\w.]+$|^[*{0,1}]$/;
+    let validVersions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
+    let validMessage = /^[^<>\\&'"]*$/;
+    if (!validMethods.includes(obj['method'])) {
+        throw Error("Invalid request header: Invalid Method");
+    }
+    let a = obj['uri'] === undefined;
+    let b = obj['uri'] === undefined || !validUri.test(obj['uri']);
+    let c = validUri.test(obj['uri']);
+    if (obj['uri'] === undefined || !validUri.test(obj['uri'])) {
+        throw Error("Invalid request header: Invalid URI");
+    }
+    if (!validVersions.includes(obj['version'])) {
+        throw Error("Invalid request header: Invalid Version");
+    }
+    if (obj['message'] === undefined || !validMessage.test(obj['message'])) {
+        throw Error("Invalid request header: Invalid Message");
+    }
+    return obj;
+}
