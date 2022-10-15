a3698806a7da28a8fffce0d2d30f87752614301b
Fikata12 <filipkarabashev@gmail.com>
2022-10-16 02:25:49 +0300

    Add exercises from 14. Unit Testing and Error Handling - Exercise

---
+++ 04. JS Advanced/JS Advanced/14. Unit Testing and Error Handling - Exercise/02. Even Or Odd/test/index.test.js
@@ -0,0 +1,40 @@
+const { expect } = require("chai");
+const { isOddOrEven } = require("..");
+describe('Test', () => {
+
+    it("should return undefined if input isn't string", () => {
+        // Arrange
+        let inputs = [1, [], {}];
+
+        for (const input of inputs) {
+            // Act
+            let result = isOddOrEven(input);
+
+            // Assert
+            expect(result).to.be.undefined;
+        }
+    });
+
+    it("should return 'even' if input is string and even", () => {
+        // Arrange
+        let input = 'Hi';
+
+        // Act
+        let result = isOddOrEven(input);
+
+        // Assert
+        expect(result).to.equal('even');
+    });
+
+    it("should return 'odd' if input is string and odd", () => {
+        // Arrange
+        let input = 'Hii';
+
+        // Act
+        let result = isOddOrEven(input);
+
+        // Assert
+        expect(result).to.equal('odd');
+    });
+
+});
