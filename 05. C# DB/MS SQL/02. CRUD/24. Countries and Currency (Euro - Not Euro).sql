9c5c9abb4d424393129c42916971658ff4239a2e
Fikata12 <filipkarabashev@gmail.com>
2023-01-16 21:05:24 +0200

    Add exercises from 02. CRUD

--- 05. C# DB/MS SQL/02. CRUD/24. Countries and Currency (Euro - Not Euro).sql
+++ 05. C# DB/MS SQL/02. CRUD/24. Countries and Currency (Euro - Not Euro).sql
@@ -1,3 +1,7 @@
-SELECT [CountryName], [CountryCode], CASE WHEN [CurrencyCode] = 'EUR' THEN 'Euro' ELSE 'Not Euro' END AS [Currency]
+SELECT [CountryName], [CountryCode], 
+CASE [CurrencyCode] 
+WHEN 'EUR' THEN 'Euro' 
+ELSE 'Not Euro' 
+END AS [Currency]
 FROM [Countries]
 ORDER BY CountryName ASC;
