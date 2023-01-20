275daabafc5bff2ac3749618029f9bdef6883e78
Fikata12 <filipkarabashev@gmail.com>
2023-01-21 01:47:23 +0200

    Add exercises from 04. Built-in Functions

---
+++ 05. C# DB/MS SQL/04. Built-in Functions/11. Find All Employees with Rank 2.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM (SELECT EmployeeID, FirstName, LastName, Salary, DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+    FROM Employees) AS Rank_Employees_by_Salary
+WHERE Salary BETWEEN 10000 AND 50000 AND Rank = 2
+ORDER BY Salary DESC;
