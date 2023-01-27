bab70fc3395a1d4a981c56e6fbcdff64c777e91c
Fikata12 <filipkarabashev@gmail.com>
2023-01-27 03:11:11 +0200

    Add exercises from 05. Subqueries and Joins

---
+++ 05. C# DB/MS SQL/05. Subqueries and Joins/14. Countries With or Without Rivers.sql
@@ -0,0 +1,7 @@
+SELECT TOP 5
+    CountryName, RiverName
+FROM Countries AS c
+    LEFT JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
+    LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
+    WHERE c.ContinentCode = 'AF'
+ORDER BY CountryName;
