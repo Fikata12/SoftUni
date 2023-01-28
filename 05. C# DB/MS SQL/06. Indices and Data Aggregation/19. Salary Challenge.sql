f91024f63dab7af2ab7a2f5baa0913ac0a115bd5
Fikata12 <filipkarabashev@gmail.com>
2023-01-29 00:24:56 +0200

    Add exercises from 06. Indices and Data Aggregation

--- 05. C# DB/MS SQL/06. Indices and Data Aggregation/19. Salary Challenge.sql
+++ 05. C# DB/MS SQL/06. Indices and Data Aggregation/19. Salary Challenge.sql
@@ -1,8 +1,7 @@
 SELECT TOP 10
-    FirstName, LastName, e.DepartmentID
-FROM Employees AS e
-    JOIN (SELECT DepartmentID, AVG(Salary) AS AverageSalary
-    FROM Employees
-    GROUP BY DepartmentID) AS DepAvgSal ON e.DepartmentID = DepAvgSal.DepartmentID
-WHERE Salary > DepAvgSal.AverageSalary
+    FirstName, LastName, DepartmentID
+FROM Employees AS e1
+WHERE Salary > (SELECT AVG(Salary)
+                FROM Employees AS e2
+                WHERE e1.DepartmentID = e2.DepartmentID)
 ORDER BY DepartmentID;
