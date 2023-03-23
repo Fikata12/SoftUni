a268b70d1cb882dcaa814f93fef8753cebfbef95
Fikata12 <filipkarabashev@gmail.com>
2023-03-23 22:31:22 +0200

    Add exercises from 05. JavaScript Object Notation - JSON

--- 05. C# DB/Entity Framework Core/05. JavaScript Object Notation - JSON/CarDealer/CarDealer/StartUp.cs
+++ 05. C# DB/Entity Framework Core/05. JavaScript Object Notation - JSON/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,37 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext context = new CarDealerContext();
 
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/suppliers.json");
+            Console.WriteLine(ImportSuppliers(context, inputJson));
+        }
+
+        // 09. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<Supplier[]>(inputJson)!;
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+           return $"Successfully imported {suppliers.Length}.";
+        }
+
+        // 10. Import Parts
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            throw new NotImplementedException();
         }
     }
 }
