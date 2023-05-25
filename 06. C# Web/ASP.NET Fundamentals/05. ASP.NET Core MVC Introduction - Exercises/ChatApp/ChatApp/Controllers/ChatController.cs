5437897ad3994f4630f2fbf705e9ea015f28a149
Fikata12 <filipkarabashev@gmail.com>
2023-05-26 01:42:58 +0300

    Add exercises from 05. ASP.NET Core MVC Introduction - Exercise

---
+++ 06. C# Web/ASP.NET Fundamentals/05. ASP.NET Core MVC Introduction - Exercises/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,43 @@
+﻿using ChatApp.Models.Chat;
+using ChatApp.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+	public class ChatController : Controller
+	{
+		private static List<KeyValuePair<string, string>> messages = 
+			new List<KeyValuePair<string, string>>();
+
+		public IActionResult Index()
+		{
+			if (messages.Count < 1)
+			{
+				return View(new ChatViewModel());
+			}
+
+			var chatModel = new ChatViewModel()
+			{
+				Messages = messages
+				.Select(m => new MessageViewModel()
+				{
+					Sender = m.Key,
+					MessageText = m.Value
+				})
+				.ToList()
+			};
+
+			return View(chatModel);
+		}
+
+		public IActionResult Send(ChatViewModel chat)
+		{
+			var newMessage = chat.CurrentMessage;
+
+			messages.Add(new KeyValuePair<string, string>
+				(newMessage.Sender, newMessage.MessageText));
+
+			return RedirectToAction("Index");
+		}
+	}
+}
