4dadd20c36da65ab800491cf1c4c4b70df1116d3
Filip Karabashev <filipkarabashev@gmail.com>
2023-09-14 00:10:23 +0300

    Add exercises from 02. Linear Data Structures - Exercise

---
+++ Data Structures Fundamentals (with C#)/02. Linear Data Structures - Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,57 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            Stack<char> openingBrackets = new Stack<char>();
+
+            Dictionary<char, char> validBrackets = new Dictionary<char, char>
+            {
+                { '{', '}' },
+                { '[', ']' },
+                { '(', ')' }
+            };
+
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                if (validBrackets.Keys.Contains(parentheses[i]))
+                {
+                    openingBrackets.Push(parentheses[i]);
+                    continue;
+                }
+
+                try
+                {
+                    if (validBrackets.Values.Contains(parentheses[i]))
+                    {
+                        if (validBrackets.Contains(new KeyValuePair<char, char>(openingBrackets.Peek(), parentheses[i])))
+                        {
+                            openingBrackets.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        continue;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+
+            if (openingBrackets.Count != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
